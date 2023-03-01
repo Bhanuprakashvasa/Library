c15dcaee2cec98911eda5024b2bafc2b021be6ad
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-03-01 12:57:30 +0530

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1 className="mt-3 text-danger">🙏Welcome to VBP Library 📘</h1>
+      <img className="mt-5" src="https://c8.alamy.com/comp/ET0D3C/psg-institute-of-management-library-coimbatore-tamil-nadu-india-asia-ET0D3C.jpg"
+       class="rounded mx-auto d-block" alt="" />
+    </div>
+  )
+}
+
+export default Dashboard
